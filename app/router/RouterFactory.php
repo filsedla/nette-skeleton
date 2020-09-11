d5325ae2ab5ac8ef67016c673400128c5b5f0fcc
Filip Sedláček <filsedla@gmail.com>
2020-09-11 09:08:37 +0200

    RouterFactory changed to older style of adding routes

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -1,19 +1,20 @@
 <?php declare(strict_types = 1);
 
-namespace App\Router;
+namespace App;
 
-use Nette;
+use Nette\Application\Routers\Route;
 use Nette\Application\Routers\RouteList;
-
 
 final class RouterFactory
 {
-    use Nette\StaticClass;
+    use \Nette\StaticClass;
 
     public static function createRouter(): RouteList
     {
         $router = new RouteList;
-        $router->addRoute('<presenter>/<action>[/<id>]', 'Home:default');
+
+        $router[] = new Route('<presenter>/<action>[/<id>]', 'Home:default');
+
         return $router;
     }
 }
